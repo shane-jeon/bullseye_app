f8875ad0f559df21b932e1617c487d9fb8d22b67
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-01-06 14:56:46 -0800

    Polishing 'Bullseye app', added 'start over' functionality, and app now successfully displays score and round

--- app/src/main/java/com/sJeon/bullseye/MainActivity.kt
+++ app/src/main/java/com/sJeon/bullseye/MainActivity.kt
@@ -12,23 +12,32 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var sliderValue = 0
-    private var targetValue = Random.nextInt(1, 100)
+    private var targetValue = newTargetValue()
+    private var totalScore = 0
+    private var currentRound = 1
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-//        Displays random integer for "targetTextView"
-        binding.targetTextView.text = targetValue.toString()
+        startNewGame()
 
 //        LISTENER (for button)
         binding.hitMeButton.setOnClickListener {
-            Log.i("Button Click Event", "You clicked the Hit me Button")
             showResult()
+            totalScore += pointsForCurrentRound()
+            binding.gameScoreTextView?.text = totalScore.toString()
+//            currentRound += 1
+//            binding.gameRoundTextView?.text = currentRound.toString()
+        }
+
+        binding.startOverButton?.setOnClickListener {
+            startNewGame()
         }
 
 //        LISTENER (for seekbar)
@@ -49,16 +58,49 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun differenceAmount() = abs(targetValue - sliderValue)
+//    private fun bonusPointsPerfectScore() = abs(differenceAmount() + 100)
+//    private fun bonusPointsCloseScore() = abs(differenceAmount() + 50)
+
+    private fun newTargetValue() = Random.nextInt(1,100)
     private fun pointsForCurrentRound(): Int {
 
+//        immutable
         val maxScore = 100
-        val difference = abs(targetValue - sliderValue)
-        return maxScore - difference
+//        immutable
+        val difference = differenceAmount()
+        var bonus = 0
+
+        if (difference == 0) {
+            bonus = 100
+//            return bonusPointsPerfectScore()
+        } else if (difference == 1) {
+            bonus = 50
+        }
+//         else if (difference == differenceAmount() + 1) || (difference == differenceAmount() - 1) {
+//            bonusPointsCloseScore()
+//        }
+
+        return maxScore - difference + bonus
+    }
+
+    private fun startNewGame() {
+        totalScore = 0
+        currentRound = 1
+        sliderValue = 50
+        targetValue = newTargetValue()
+
+        binding.gameScoreTextView?.text = totalScore.toString()
+        binding.gameRoundTextView?.text = currentRound.toString()
+        binding.targetTextView.text = targetValue.toString()
+        binding.seekBar.progress = sliderValue
     }
 
     private fun showResult() {
 //        Pop up message
-        val dialogTitle = getString(R.string.result_dialog_title)
+//        immutable " "
+//        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogTitle = alertTitle()
         val dialogMessage = getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
 //        val dialogMessage = "The slider's value is $sliderValue"
 
@@ -69,8 +111,30 @@ class MainActivity : AppCompatActivity() {
         builder.setMessage(dialogMessage)
         builder.setPositiveButton(R.string.result_dialog_button_text) {dialog, _ ->
             dialog.dismiss()
+//            didn't add following code block
+            targetValue = newTargetValue()
+            binding.targetTextView.text = targetValue.toString()
+
+            currentRound += 1
+            binding.gameRoundTextView?.text = currentRound.toString()
         }
 
         builder.create().show()
+    }
+
+    private fun alertTitle(): String {
+        val difference = differenceAmount()
+
+        val title: String = if (difference == 0) {
+            getString(R.string.alert_title_1)
+            } else if (difference < 5) {
+                getString(R.string.alert_title_2)
+        } else if (difference <= 10) {
+            getString(R.string.alert_title_3)
+        } else {
+            getString(R.string.alert_title_4)
+        }
+
+        return title
     }
 }
